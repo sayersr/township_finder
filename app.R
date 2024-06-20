a717b1ede5541066377060547fcb527b1e28f9eb
Bobby Sayers <sayersr@mac.com>
2024-06-20 08:52:34 -0400

    added map functionality

--- app.R
+++ app.R
@@ -3,6 +3,7 @@ library(shinyWidgets)
 library(sf)
 library(tidygeocoder)
 library(dplyr)
+library(leaflet)  # Add this line to load the leaflet package
 
 # Load shapefiles
 township_shapes <- st_read("./data/Long_Island_Cities_Towns.shp")
@@ -13,19 +14,21 @@ township_shapes <- st_transform(township_shapes, crs = 4326)
 county_shapes <- st_transform(county_shapes, crs = 4326)
 
 ui <- fluidPage(
-  titlePanel("Address Geocoder"),
+  titlePanel("Township Finder"),
   sidebarLayout(
     sidebarPanel(
-      textInput("street", "Street Address"),
-      textInput("city", "City"),
+      textInput("street", "Street Address (ex. 123 Kings Park Blvd)"),
+      textInput("city", "City in New York"),
       actionButton("submit", "Geocode")
     ),
     mainPanel(
       textOutput("matched_address"),
       textOutput("township"),
-      textOutput("county")
+      textOutput("county"),
+      leafletOutput("map")  # Add a leaflet output
     )
-  )
+  ),
+  HTML("<p>This app helps you find the township/city and county of a given address on Long Island. There are a few zip codes around here that span multiple towns. For more information on administrative divisions in New York, visit <a href='https://en.wikipedia.org/wiki/Administrative_divisions_of_New_York_(state)#Town' target='_blank'>this Wikipedia page</a>.</p>")
 )
 
 server <- function(input, output, session) {
@@ -61,10 +64,17 @@ server <- function(input, output, session) {
       paste("Full Address: ", matched_address, sep = "")
     })
     output$township <- renderText({
-      paste("Township/City: ", ifelse(length(township) > 0, township, "Not found"), sep = "")
+      paste("Township/City: ", ifelse(length(township) > 0, township, "Address is outside of Long Island or not found"), sep = "")
     })
     output$county <- renderText({
-      paste("County: ", ifelse(length(county) > 0, county, "Not found"), sep = "")
+      paste("County: ", ifelse(length(county) > 0, county, "Address is outside of New York or not found"), sep = "")
+    })
+    
+    # Render map
+    output$map <- renderLeaflet({
+      leaflet() %>%
+        addTiles() %>%
+        addMarkers(lng = lon, lat = lat, popup = matched_address)
     })
   })
 }
